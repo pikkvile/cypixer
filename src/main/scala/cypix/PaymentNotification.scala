2f4ed7a84107497ea0f91d0ed0ea5a48af659b9b
Ilya Dubov <pikkvile@gmail.com>
2017-08-18 19:56:11 +0300

    Split payment notification

--- src/main/scala/cypix/PaymentNotification.scala
+++ src/main/scala/cypix/PaymentNotification.scala
@@ -7,32 +7,66 @@ import com.netaporter.uri.Uri.parse
 
 import Utils._
 
-case class PaymentNotification(serviceId: String, orderId: String, processingStatus: String, errorCode: Option[Int],
-                               price: BigDecimal, priceRub: BigDecimal, currency: Currency, share: BigDecimal,
-                               shareRub: BigDecimal, transactionDate: DateTime, transactionId: String,
-                               paymentMethod: PaymentMethod)
+trait PaymentNotification
+case class PaymentProcessed(serviceId: String, orderId: String, processingStatus: String,
+                            price: BigDecimal, priceRub: BigDecimal, currency: Currency, share: BigDecimal,
+                            shareRub: BigDecimal, transactionDate: DateTime, transactionId: String,
+                            paymentMethod: PaymentMethod) extends PaymentNotification
+case class PaymentFailed(serviceId: Option[String], orderId: Option[String], processingStatus: String, errorCode: Int,
+                         price: Option[BigDecimal], priceRub: Option[BigDecimal], currency: Option[Currency],
+                         share: Option[BigDecimal], shareRub: Option[BigDecimal], transactionDate: Option[DateTime],
+                         transactionId: Option[String], paymentMethod: Option[PaymentMethod]) extends PaymentNotification
+
 object PaymentNotification {
 
   def unpack(payload: String, secret: String): PaymentNotification = {
-    val queryMap = parse(payload).query.params.toMap
+
+    val queryMap = parse(payload).query.params.toMap.withDefaultValue(None)
     val get = (key: String) => {
       queryMap(key) match {
         case Some(s) => s
         case None => throw new IllegalArgumentException(s"Can not find $key in $payload")
       }
     }
+    val getOrEmpty = (key: String) => {
+      queryMap(key) match {
+        case Some(s) => s
+        case None => ""
+      }
+    }
+
     val processingStatus = get("processing_status")
     val signature = get("hash")
-    val calculatedSignature = hash(get("service_id") + get("transaction_id") + get("service_id") +
-      processingStatus + get("price") + get("price_rub") + get("currency") + get("share") + get("share_rub") +
-      get("transaction_date") + get("payment_method_id") + secret)
-    val errorCode = if (processingStatus == "FAILED") Some(get("error_code").toInt) else None
-    if (signature == calculatedSignature)
-      PaymentNotification(get("service_id"), get("order_id"), get("processing_status"), errorCode,
-                          BigDecimal(get("price")), BigDecimal(get("price_rub")), Currency.getInstance(get("currency")),
-                          BigDecimal(get("share")), BigDecimal(get("share_rub")),
-                          new DateTime(get("transaction_date").replace(" ", "T")),
-                          get("transaction_id"), PaymentMethod.of(get("payment_method_id").toInt))
-    else throw new SecurityException(s"Hash check failed for $payload")
+
+    processingStatus match {
+
+      case "PROCESSED" =>
+        val calculatedSignature = hash(get("service_id") + get("transaction_id") + get("service_id") +
+          processingStatus + get("price") + get("price_rub") + get("currency") + get("share") + get("share_rub") +
+          get("transaction_date") + get("payment_method_id") + secret)
+        if (signature == calculatedSignature)
+          PaymentProcessed(get("service_id"), get("order_id"), get("processing_status"),
+            BigDecimal(get("price")), BigDecimal(get("price_rub")), Currency.getInstance(get("currency")),
+            BigDecimal(get("share")), BigDecimal(get("share_rub")),
+            new DateTime(get("transaction_date").replace(" ", "T")),
+            get("transaction_id"), PaymentMethod.of(get("payment_method_id").toInt))
+        else throw new SecurityException(s"Hash check failed for $payload")
+
+      case "FAILED" =>
+        val calculatedSignature = hash(getOrEmpty("service_id") + getOrEmpty("transaction_id") +
+          getOrEmpty("order_id") + processingStatus + getOrEmpty("price") + getOrEmpty("price_rub") +
+          getOrEmpty("currency") + getOrEmpty("share") + getOrEmpty("share_rub") + getOrEmpty("transaction_date") +
+          getOrEmpty("payment_method_id") + secret)
+        if (signature == calculatedSignature)
+          PaymentFailed(queryMap("service_id"), queryMap("order_id"), get("processing_status"), get("error_code").toInt,
+            queryMap("price").map(s => BigDecimal(s)), queryMap("price_rub").map(s => BigDecimal(s)),
+            queryMap("currency").map(Currency.getInstance),
+            queryMap("share").map(s => BigDecimal(s)), queryMap("share_rub").map(s => BigDecimal(s)),
+            queryMap("transaction_date").map(s => new DateTime(s.replace(" ", "T"))),
+            queryMap("transaction_id"), queryMap("payment_method_id").map(s => PaymentMethod.of(s.toInt)))
+        else throw new SecurityException(s"Hash check failed for $payload")
+
+      case _ => throw new IllegalArgumentException(s"Unknown processingStatus $processingStatus")
+    }
   }
 }
--- src/test/scala/cypix/PaymentNotificationSpec.scala
+++ src/test/scala/cypix/PaymentNotificationSpec.scala
@@ -7,23 +7,43 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class PaymentNotificationSpec extends FlatSpec with Matchers {
 
-  "PaymentNotification" should "be parsed" in {
+  "PaymentProcessed" should "be parsed" in {
     val request = "http://some.ru/cypix/?service_id=5432&order_id=12345678asdfgh&processing_status=PROCESSED" +
       "&price=120.20&price_rub=120.20&currency=RUB&share=120.20&share_rub=120.20&transaction_date=2016-03-08 00:51:52" +
       "&transaction_id=2016030&payment_method_id=2&hash=4804824075c79f3ef22fab36a68eb566"
-    val unpacked = PaymentNotification.unpack(request, "sdfsdf")
-    unpacked.serviceId should be ("5432")
-    unpacked.orderId should be ("12345678asdfgh")
-    unpacked.processingStatus should be ("PROCESSED")
-    unpacked.price should be (BigDecimal(120.20))
-    unpacked.priceRub should be (BigDecimal(120.20))
-    unpacked.share should be (BigDecimal(120.20))
-    unpacked.shareRub should be (BigDecimal(120.20))
-    unpacked.currency should be (Currency.getInstance("RUB"))
-    unpacked.transactionDate should be (new DateTime("2016-03-08T00:51:52"))
-    unpacked.transactionId should be ("2016030")
-    unpacked.paymentMethod should be (BANK_CARD)
-    unpacked.errorCode should be (None)
+    PaymentNotification.unpack(request, "sdfsdf") match {
+      case (unpacked: PaymentProcessed) =>
+        unpacked.serviceId should be ("5432")
+        unpacked.orderId should be ("12345678asdfgh")
+        unpacked.processingStatus should be ("PROCESSED")
+        unpacked.price should be (BigDecimal(120.20))
+        unpacked.priceRub should be (BigDecimal(120.20))
+        unpacked.share should be (BigDecimal(120.20))
+        unpacked.shareRub should be (BigDecimal(120.20))
+        unpacked.currency should be (Currency.getInstance("RUB"))
+        unpacked.transactionDate should be (new DateTime("2016-03-08T00:51:52"))
+        unpacked.transactionId should be ("2016030")
+        unpacked.paymentMethod should be (BANK_CARD)
+    }
+  }
+
+  "PaymentFailed" should "be parsed" in {
+    val request = "http://some.ru/cypix/?service_id=5432&order_id=12345678asdfgh&processing_status=FAILED" +
+      "&error_code=4&hash=417ba9fee156403c79a4e49b9b13bb5a"
+    PaymentNotification.unpack(request, "sdfsdf") match {
+      case (unpacked: PaymentFailed) =>
+        unpacked.serviceId should be (Option("5432"))
+        unpacked.orderId should be (Option("12345678asdfgh"))
+        unpacked.processingStatus should be ("FAILED")
+        unpacked.price should be (None)
+        unpacked.priceRub should be (None)
+        unpacked.share should be (None)
+        unpacked.shareRub should be (None)
+        unpacked.currency should be (None)
+        unpacked.transactionDate should be (None)
+        unpacked.transactionId should be (None)
+        unpacked.paymentMethod should be (None)
+    }
   }
 
   "SecurityException" should "be thrown" in {
